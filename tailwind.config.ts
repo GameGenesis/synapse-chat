8b65f13af0db9fccfaa1c0573905875d329a275b
Rayan Kaissi <rayan.kaissi@techguilds.com>
2024-07-19 19:10:58 -0400

    Update packages and add new shadcn components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -50,6 +50,7 @@ const config: Config = {
             }
           },
       borderRadius: {
+            "full": "9999px",
             "lg": "var(--radius)",
             "md": "calc(var(--radius) - 2px)",
             "sm": "calc(var(--radius) - 4px)"
