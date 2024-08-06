5c26c7619b17d468d7dec9f1afcb4d7035ec38d7
Rayan Kaissi <rayan.kaissi@techguilds.com>
2024-08-06 19:33:14 -0400

    Use Groq llama 3.1 8b to summarize messages

--- lib/utils/message-manager.ts
+++ lib/utils/message-manager.ts
@@ -5,9 +5,9 @@ const MAX_SUMMARY_LENGTH_WORDS = 50;
 
 const summarizeMessages = async (messages: CoreMessage[]) => {
     const { text } = await generateText({
-        model: getModel(models.gpt4omini),
+        model: getModel(models.llama31_8b),
         system: `
-You will be given a series of messages within <messages> tags from a conversation between a user and an AI assistant. Your task is to provide a concise summary of these messages.
+You will be given a series of messages within <messages> tags from a conversation between a user and an AI assistant. Some of these messages are previous summaries of the conversation. Your task is to provide a concise summary of these messages.
 
 Please summarize the conversation above in no more than ${MAX_SUMMARY_LENGTH_WORDS} words. Make sure to include key information and main points discussed. Your summary must be shorter than the original message content.
 
@@ -23,16 +23,20 @@ ${JSON.stringify(messages)}
         maxTokens: 512
     });
     const content = `This is a summary of past conversation messages between the user and assistant:\n${text}`;
+    console.log(content);
 
     return { role: "assistant", content } as CoreMessage;
 };
 
+// When summarizing, usually takes around 1s (0.9s - 1.5s) to process with gpt-4o-mini
+// When summarizing, usually takes around 0.5s (0.3s - 1s) to process with Groq Llama 3.1 8B
 export const limitMessages = async (
     messages: CoreMessage[],
     limit: number = 6,
     summarizeRest: boolean = true,
     summary_threshold: number = 1
 ) => {
+    const startTime = performance.now()
     if (messages.length <= limit) {
         return messages;
     }
@@ -56,6 +60,9 @@ export const limitMessages = async (
     // get the last (limit) number of messages in the conversation
     const recentMessages = messages.slice(-thresholdLimit);
     newMessages.push(...recentMessages);
+
+    const endTime = performance.now()
+    console.log(`Call to limitMessages took ${endTime - startTime} milliseconds`)
 
     return newMessages;
 };
