d5e6d2915f066c5549bea675e8f4aa4005ac2dcb
Rayan Kaissi <gamegenesisdev@gmail.com>
2024-08-20 21:57:44 -0400

    update evaluateJavaScript func to add timeout and context

--- lib/tools/eval.ts
+++ lib/tools/eval.ts
@@ -1,13 +1,14 @@
 import vm from "vm";
 
-const executeJavaScript = (code: string) => {
+const executeJavaScript = (code: string, timeout = 2000) => {
     try {
-        const result = vm.runInNewContext(code);
+        const context = vm.createContext({});
+        const result = vm.runInNewContext(code, context, { timeout: timeout });
         return { success: true, result };
     } catch (error) {
         const errorMessage = (error as Error).message || error;
         return { success: false, error: errorMessage };
     }
-}
+};
 
 export default executeJavaScript;
